50977a6d34cfc9a2e6e8c292681ccb84838026d9
JestrabikR <jestrab2003@gmail.com>
2026-05-17 21:27:08 +0200

    Manually spawning enemies

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,9 +1,6 @@
 using Godot;
 using System;
 
-using Godot;
-using System;
-
 public partial class GameManager : Node2D
 {
     private int _gold = 100;
@@ -12,10 +9,15 @@ public partial class GameManager : Node2D
     private Label _goldLabel;
     private Label _livesLabel;
 
+    private PackedScene _enemyScene = GD.Load<PackedScene>("res://enemy.tscn");
+    private Path2D _enemyPath;
+
     public override void _Ready()
     {
         _goldLabel = GetNode<Label>("%GoldLabel");
         _livesLabel = GetNode<Label>("%LivesLabel");
+
+        _enemyPath = GetNode<Path2D>("%EnemyPath");
 
         UpdateUi();
 
@@ -26,6 +28,14 @@ public partial class GameManager : Node2D
     {
         _goldLabel.Text = $"Zlato: {_gold}";
         _livesLabel.Text = $"Životy: {_lives}";
+    }
+
+    public void OnSpawnButtonPressed()
+    {
+        GD.Print("Spawning enemy...");
+        var enemyInstance = _enemyScene.Instantiate<Enemy>();
+
+        _enemyPath.AddChild(enemyInstance);
     }
 
     public void AddGold(int amount)
